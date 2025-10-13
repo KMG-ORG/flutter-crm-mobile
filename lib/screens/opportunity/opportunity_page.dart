971daccba9ac2ebcc1c9560c91399d3b5809b7e4
Veeresh <veeresh.thakur@spectraltech.ai>
2025-10-13 13:19:48 +0530

    changes for opportunity issue fixing.

--- lib/screens/opportunity/opportunity_page.dart
+++ lib/screens/opportunity/opportunity_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:crm_mobile/services/api_service.dart';
-
+import 'package:intl/intl.dart';
 
 class OpportunityPage extends StatefulWidget {
   final VoidCallback onClose;
@@ -10,14 +10,13 @@ class OpportunityPage extends StatefulWidget {
   State<OpportunityPage> createState() => _OpportunityPageState();
 }
 
-class _OpportunityPageState extends State<OpportunityPage>{
-
+class _OpportunityPageState extends State<OpportunityPage> {
   bool isLoading = true;
   bool isLoadingMore = false;
   bool hasMore = true;
   bool showSearchBar = false;
   int pageNumber = 1;
-  final int pageSize = 200;
+  final int pageSize = 100;
   int totalCount = 0;
 
   final ScrollController _scrollController = ScrollController();
@@ -41,7 +40,7 @@ class _OpportunityPageState extends State<OpportunityPage>{
   void _onScroll() {
     // Trigger only if near bottom, not already loading, and more data exists
     if (_scrollController.position.pixels >=
-        _scrollController.position.maxScrollExtent - 200 &&
+            _scrollController.position.maxScrollExtent - 200 &&
         !isLoadingMore &&
         hasMore) {
       pageNumber++;
@@ -49,12 +48,10 @@ class _OpportunityPageState extends State<OpportunityPage>{
     }
   }
 
-
   void onSearchPressed() {
     if (showSearchBar) {
       fetchOpportutnity(1, _searchController.text.trim());
     } else {
-
       setState(() {
         showSearchBar = true;
       });
@@ -95,12 +92,12 @@ class _OpportunityPageState extends State<OpportunityPage>{
       };
 
       final response =
-      await apiService.getOpportunity(payload) as Map<String, dynamic>;
+          await apiService.getOpportunity(payload) as Map<String, dynamic>;
 
       final List<Map<String, dynamic>> newOpportunity =
-      (response['data'] as List<dynamic>? ?? [])
-          .map((item) => Map<String, dynamic>.from(item as Map))
-          .toList();
+          (response['data'] as List<dynamic>? ?? [])
+              .map((item) => Map<String, dynamic>.from(item as Map))
+              .toList();
 
       totalCount =
           int.tryParse(response['totalCount']?.toString() ?? '') ?? totalCount;
@@ -123,73 +120,89 @@ class _OpportunityPageState extends State<OpportunityPage>{
     }
   }
 
+  Future<void> _refreshOpportutnity() async {
+    pageNumber = 1;
+    hasMore = true;
+    await fetchOpportutnity(pageNumber);
+  }
+
+  String _formatDate(String? dateString) {
+    if (dateString == null || dateString.isEmpty) return "N/A";
+    try {
+      final parsedDate = DateTime.parse(dateString);
+      return DateFormat('dd/MM/yyyy').format(parsedDate);
+    } catch (e) {
+      return "N/A"; // in case of invalid format
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          title: showSearchBar
-              ? TextField(
-            controller: _searchController,
-            autofocus: true,
-            decoration: InputDecoration(
-              hintText: "Search Opportunity",
-              hintStyle: const TextStyle(color: Colors.white70),
-              border: InputBorder.none,
-            ),
-            style: const TextStyle(color: Colors.white),
-
-          )
-              : const Text(
-            "Opportunity",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
+      appBar: AppBar(
+        elevation: 0,
+        title: showSearchBar
+            ? TextField(
+                controller: _searchController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: "Search Opportunity",
+                  hintStyle: const TextStyle(color: Colors.white70),
+                  border: InputBorder.none,
+                ),
+                style: const TextStyle(color: Colors.white),
+                onSubmitted: (value) => fetchOpportutnity(1, value),
+              )
+            : const Text(
+                "Opportunity",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: widget.onClose,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              showSearchBar ? Icons.check : Icons.search,
               color: Colors.white,
             ),
+            onPressed: onSearchPressed,
           ),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: widget.onClose,
-          ),
-          actions: [
+          if (showSearchBar)
             IconButton(
-              icon: Icon(
-                showSearchBar ? Icons.check : Icons.search,
-                color: Colors.white,
+              icon: const Icon(Icons.close, color: Colors.white),
+              onPressed: onCancelSearch,
+            )
+          else
+            Container(
+              margin: const EdgeInsets.only(right: 12),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(8),
               ),
-              onPressed: onSearchPressed,
+              // child: IconButton(
+              //   icon: const Icon(Icons.add, color: Colors.white),
+              //   onPressed: () {},
+              // ),
             ),
-            if (showSearchBar)
-              IconButton(
-                icon: const Icon(Icons.close, color: Colors.white),
-                onPressed: onCancelSearch,
-              )
-            else
-              Container(
-                margin: const EdgeInsets.only(right: 12),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                // child: IconButton(
-                //   icon: const Icon(Icons.add, color: Colors.white),
-                //   onPressed: () {},
-                // ),
-              ),
-          ],
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xFF5733C7), Color(0xFF9A24C3)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+        ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF5733C7), Color(0xFF9A24C3)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
         ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -210,19 +223,37 @@ class _OpportunityPageState extends State<OpportunityPage>{
 
             // List of opportunities
             Expanded(
-              child: ListView.builder(
-                itemCount: opportunities.length,
-                itemBuilder: (context, index) {
-                  final opp = opportunities[index];
-                  return OpportunityCard(
-                    name: opp['name']!,
-                    stage: opp['stage']!,
-                    amount: opp['amount']!,
-                    date: opp['date']!,
-                    owner: opp['owner']!,
-                  );
-                },
-              ),
+              child: isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : opportunities.isEmpty
+                  ? const Center(child: Text("No opportunities found"))
+                  : RefreshIndicator(
+                      onRefresh: _refreshOpportutnity,
+                      child: ListView.builder(
+                        controller: _scrollController,
+                        itemCount:
+                            opportunities.length + (isLoadingMore ? 1 : 0),
+                        itemBuilder: (context, index) {
+                          if (index < opportunities.length) {
+                            final opp = opportunities[index];
+                            return OpportunityCard(
+                              name: opp['name'] ?? "N/A",
+                              stage: opp['stage'] ?? "N/A",
+                              //amount: opp['amount']!,
+                              amount: (opp['amount'] ?? "0").toString(),
+                              //date: opp['closingDate'] ?? "N/A",
+                              date: _formatDate(opp['closingDate']),
+                              owner: opp['owner'] ?? "N/A",
+                            );
+                          } else {
+                            return const Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Center(child: CircularProgressIndicator()),
+                            );
+                          }
+                        },
+                      ),
+                    ),
             ),
           ],
         ),
@@ -274,40 +305,67 @@ class OpportunityCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(name,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w600)),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
                 const SizedBox(height: 6),
                 Row(
                   children: [
                     const Icon(Icons.assignment, color: purpleColor, size: 16),
                     const SizedBox(width: 4),
-                    Text(stage,
-                        style: const TextStyle(
-                            color: purpleColor, fontWeight: FontWeight.w500)),
+                    Text(
+                      stage,
+                      style: const TextStyle(
+                        color: purpleColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ],
                 ),
                 const SizedBox(height: 6),
                 Row(
                   children: [
-                    const Icon(Icons.attach_money,
-                        color: purpleColor, size: 16),
+                    const Icon(
+                      Icons.attach_money,
+                      color: purpleColor,
+                      size: 16,
+                    ),
                     const SizedBox(width: 4),
-                    Text(amount,
-                        style: const TextStyle(fontWeight: FontWeight.w500)),
+                    Text(
+                      amount,
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
                   ],
                 ),
                 const SizedBox(height: 6),
                 Row(
                   children: [
-                    const Icon(Icons.calendar_today,
-                        color: purpleColor, size: 16),
+                    const Icon(
+                      Icons.calendar_today,
+                      color: purpleColor,
+                      size: 16,
+                    ),
                     const SizedBox(width: 4),
                     Text(date),
-                    const SizedBox(width: 10),
+                    // const SizedBox(width: 10),
+                    // const Icon(Icons.person, color: purpleColor, size: 16),
+                    // const SizedBox(width: 4),
+                    // Text(owner),
+                  ],
+                ),
+                const SizedBox(height: 6),
+                Row(
+                  children: [
                     const Icon(Icons.person, color: purpleColor, size: 16),
                     const SizedBox(width: 4),
-                    Text(owner),
+                    Text(
+                      owner,
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
                   ],
                 ),
               ],
